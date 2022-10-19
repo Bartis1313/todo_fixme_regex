e907cadc1cd48d809477f14177d13e885fb97e07
Bartis1313 <bartis1313@gmail.com>
2022-10-20 01:45:44 +0200

    Add files

---
+++ README.md
@@ -0,0 +1,3 @@
+# fixme todo regex
+Single script to detect comments in your files. Loops every file in running directory.
+See test: https://regex101.com/r/5v4jzn/1
---
+++ no-todos.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+found=0
+
+for i in `find . -type f -name "*.c" -o -name "*.cpp" -o -name "*.h" -o -name "*.hpp" -o -name "*.sh" -o -name "*.py"`;
+do
+  if [[ $i =~ .*no-todos.sh.* ]]; then
+    continue
+  fi
+  result=(`grep -iozE '(((\/\/|#).*(todo|fixme))|(\/\*[^*]*\*+(?:[^\/*][^*]*\*+)*\/))' "$i" | grep -icE '(todo|fixme)'`)
+    if [[ $result -gt 0 ]]; then
+      echo "Found todo/fixme comment in $i"
+     $found=1
+    fi
+done
+exit $found
